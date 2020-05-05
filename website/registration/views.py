295537afcc6ea023021fe974c891fad07fcde1cb
Timo <timo.schessl@gmx.de>
2020-05-05 13:17:37 +0200

    Connection to mysql database in phpmyadmin works, inserting new user into database works!

--- website/registration/views.py
+++ website/registration/views.py
@@ -1,3 +1,6 @@
+import random
+import pymysql
+
 from django.shortcuts import render
 
 from .forms import *
@@ -23,7 +26,10 @@ def getUsername(request):
                 print("The passwords are not the same!")
             else:
                 print("The passwords are the same!")
-                
+                database = pymysql.connect(host="localhost", user="root", passwd="", database="besucher")
+                cursor = database.cursor()
+                insertNewRowIntoUserTable(usernameData,passwordData,repeatPasswordData,cursor,database)
+
 
     else:
         usernameFormObject = usernameForm()
@@ -35,3 +41,78 @@ def getUsername(request):
                    "passwordInput":passwordFormObject,
                    "repeatPasswordInput":repeatPasswordFormObject})
 
+
+
+
+def getRowOfUserTable(i,cursor):
+    cursor.execute("select * from user")
+    result = cursor.fetchall()
+
+    print(result[i])
+
+
+def getColumnOfUserTable(columnName,cursor):
+    cursor.execute("select " + columnName + " from user")
+    result = cursor.fetchall()
+
+    return result
+
+
+def deleteRowOfUserTable(i,cursor,database):
+    cursor.execute("select * from user")
+    result = cursor.fetchall()
+    print("Deleted the row: " + str(result[i]))
+
+    idOfRow = str(result[i][0])
+
+    cursor.execute("delete from user where id='" + idOfRow + "'")
+
+    database.commit()
+
+
+def deleteAllRows(cursor,database):
+    cursor.execute("delete from user")
+    database.commit()
+
+
+def insertNewRowIntoUserTable(username, password, repeatedPassword,cursor,database):
+
+    if(password != repeatedPassword):
+        print("The passwords are not the same!")
+        return None
+
+    if(checkIfUsernameExists(username,cursor)):
+        print("This username already exists!")
+        return None
+
+    id = random.randint(0, 10000000)
+
+    while (checkIfIdExists(id,cursor)):
+        id = random.randint(0, 10000000)
+
+    cursor.execute(
+        "insert into user (id,username,password) VALUES ('" + str(id) + "','" + username + "','" + password + "')")
+    database.commit()
+
+
+
+def checkIfIdExists(id,cursor):
+    idColumn = getColumnOfUserTable("id",cursor)
+
+    for element in idColumn:
+        for entry in element:
+         if(entry==id):
+            return True
+
+    return False
+
+
+def checkIfUsernameExists(username,cursor):
+    usernameColumn = getColumnOfUserTable("username",cursor)
+
+    for element in usernameColumn:
+        for entry in element:
+            if (entry == username):
+                return True
+
+    return False
